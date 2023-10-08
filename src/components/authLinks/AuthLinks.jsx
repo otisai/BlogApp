672bd119407eea74fce5e27db9297b1bd7f03aa8
Otis Alejandro <128675915+OtisAlejandro@users.noreply.github.com>
2023-10-08 15:56:21 -0500

    more comps

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,9 @@
+import styles from "./authLinks.module.css";
+
+const AuthLinks = () => {
+  return (
+    <div className={styles.container}>AuthLinks</div>
+  );
+};
+
+export default AuthLinks;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24}/>
+        <Image src="/instagram.png" alt="instagram" width={24} height={24}/>
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24}/>
+        <Image src="/youtube.png" alt="youtube" width={24} height={24}/>
+      </div>
+      <div className={styles.logo}>BlogApp</div>
+      <div className={styles.links}>
+        <Link href="/">Home</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
+  )
 };
 
 export default Navbar;
