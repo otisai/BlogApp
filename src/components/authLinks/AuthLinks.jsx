8d2e16b203fccb1af873a4be1f7a1c5e429e0a8f
Otis Alejandro <128675915+OtisAlejandro@users.noreply.github.com>
2023-10-09 19:29:57 -0500

    next auth

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,23 +1,28 @@
 "use client";
-
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
 
-  // temporary
-  const status = "notauthenticated";
+  const { status } = useSession();
 
   return (
     <>
-      {status === "notauthenticated" ? (
-        <Link href="/login" className={styles.link}>Login</Link>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write" className={styles.link}>Write</Link>
-          <span className={styles.link}>Logout</span>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -28,8 +33,8 @@ const AuthLinks = () => {
       {open && (
         <div className={styles.responsiveMenu}>
           <Link href="/">Homepage</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
           {status === "notauthenticated" ? (
             <Link href="/login">Login</Link>
           ) : (
